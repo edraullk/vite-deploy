801851b1d2d237b727968c4d2ded7c583c32fc3d
Edraullk <edraullk@gmail.com>
2023-06-05 00:28:21 -0500

    development the web 1

---
+++ src/components/FilePicker.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import CustomButton from './CustomButton'
+
+const FilePicker = ({file, setFile, readFile}) => {
+  return (
+    <div className='filepicker-container'>
+      <div className='flex-1 flex flex-col'>
+        <input
+          id='file-upload'
+          type='file'
+          accept='image/*'
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label htmlFor='file-upload' className='filepicker-label'>
+          Subir Archivo
+        </label>
+        <p className='mt-2 text-gray-500 text-xs truncate'>
+          {file === ''? "Ningún Archivo Seleccionado" : file.name}
+        </p>
+      </div>
+      <div className='mt-4 flex flex-wrap gap-36'>
+        <CustomButton
+          type="outline"
+          title="Logo"
+          handleClick={() => readFile('logo')}
+          customStyles="text-xs"
+        />
+        <CustomButton
+          type="filled"
+          title="Completo"
+          handleClick={() => readFile('full')}
+          customStyles="text-xs"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default FilePicker
